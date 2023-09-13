24f36b5c909125f7544e022b6ab43bad5fc27467
3xplosiv3 <116503274+3xplosiv3-official@users.noreply.github.com>
2023-09-13 15:34:54 +0300

    feat: add FastAPI app, run uvicorn in main.py
    Set up FastAPI application.
    Create FastAPI startup event.
    Run uvicorn.

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,28 @@
+from fastapi import FastAPI
+
+from app.core.config.settings import settings
+from app.core.database.connection import database_init
+
+
+# Set up FastAPI application
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    description=settings.PROJECT_DESCRIPTION,
+    version=settings.PROJECT_VERSION
+)
+
+
+@app.on_event("startup")
+async def startup_event():
+    """
+    Startup event for the FastAPI application.
+    """
+
+    await database_init()
+
 
 # Entry point for running the application
 if __name__ == "__main__":
+    import uvicorn
 
-    pass
+    uvicorn.run(app, host="0.0.0.0", port=8000)
