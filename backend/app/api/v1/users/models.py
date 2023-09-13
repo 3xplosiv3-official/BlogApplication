e9f4e7234cfa9eb3435d4288b9d6048a2cfb97d5
3xplosiv3 <116503274+3xplosiv3-official@users.noreply.github.com>
2023-09-14 01:28:37 +0300

    feat: add instance methods to User model
    Create instance methods set_password and verify_password in User.

--- backend/app/api/v1/users/models.py
+++ backend/app/api/v1/users/models.py
@@ -3,6 +3,8 @@ from tortoise import fields
 from app.models.base import AbstractBaseModel
 from app.models.timestamp import TimestampMixin
 
+from services.password import hash_password, verify_password
+
 
 class User(AbstractBaseModel, TimestampMixin):
 
@@ -11,3 +13,10 @@ class User(AbstractBaseModel, TimestampMixin):
     hashed_password = fields.CharField(max_length=255)
     is_active = fields.BooleanField(default=True)
 
+    def set_password(self, password: str):
+
+        self.hashed_password = hash_password(password)
+
+    def verify_password(self, password: str) -> bool:
+
+        return verify_password(password, self.hashed_password)
