7dcfb35f7ca9b7b92edd5b6a47864ebd39168ebd
3xplosiv3 <116503274+3xplosiv3-official@users.noreply.github.com>
2023-09-13 16:10:40 +0300

    feat: add orm models, add project settings
    Create list of ORM models and add models in ORM configuration.
    Add new app settings such as: name, description, version, api endpoint

--- backend/app/core/config/settings.py
+++ backend/app/core/config/settings.py
@@ -6,6 +6,12 @@ class Settings(BaseSettings):
     Application's main settings.
 
     """
+
+    PROJECT_NAME: str = "ENIGMA BLOG APPLICATION"
+    PROJECT_DESCRIPTION: str = "Backend of blog app for the test task at LZT forum"
+    PROJECT_VERSION: str = "7.7.7"
+    API_V1_ENDPOINT: str = "/api/v1"
+
     DATABASE_URL: str = "sqlite://sqlite3.db"
 
     class Config:
@@ -16,12 +22,19 @@ class Settings(BaseSettings):
 # Initialize the settings instance.
 settings = Settings()
 
+# List of models for Tortoise ORM.
+ORM_MODELS = [
+    "app.api.v1.articles.models",
+    "app.api.v1.comments.models",
+    "app.api.v1.users.models"
+]
+
 # Configuration for Tortoise ORM.
 TORTOISE_ORM = {
     "connections": {"default": settings.DATABASE_URL},
     "apps": {
         "models": {
-            "models": [''],
+            "models": ORM_MODELS,
             "default_connection": "default",
         },
     },
