dad87ee27a42a22bce1e89883ec137dba6fc91d9
3xplosiv3 <116503274+3xplosiv3-official@users.noreply.github.com>
2023-09-14 02:14:11 +0300

    feat: add articles routers
    Create routers such as to get list of articles, to get article by id, to create, update and delete articles.

--- backend/app/api/v1/articles/routes.py
+++ backend/app/api/v1/articles/routes.py
@@ -1,3 +1,34 @@
-from fastapi import APIRouter
+from fastapi import Query, Depends, APIRouter
+
 
 router = APIRouter(prefix="/articles", tags=["articles"])
+
+
+@router.get("/")
+async def list_articles():
+
+    return {}
+
+
+@router.get("/{article_id}",)
+async def retrieve_article():
+
+    return {}
+
+
+@router.post("/")
+async def create_new_article():
+
+    return {}
+
+
+@router.put("/{article_id}")
+async def update_existing_article():
+
+    return {}
+
+
+@router.delete("/{article_id}")
+async def remove_article():
+
+    return {}
