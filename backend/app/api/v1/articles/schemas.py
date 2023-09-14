aab0516c71689a4f3eb0eecbd0f46aafedb0ad7c
3xplosiv3 <116503274+3xplosiv3-official@users.noreply.github.com>
2023-09-14 15:06:25 +0300

    feat: add articles schemas
    Create empty schemas in articles such as ArticleBase,  ArticleCreate, ArticleUpdate, ArticleInDB, ArticleResponse.

---
+++ backend/app/api/v1/articles/schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+
+class ArticleBase(BaseModel):
+
+    pass
+
+
+class ArticleCreate(BaseModel):
+
+    pass
+
+
+class ArticleUpdate(BaseModel):
+
+    pass
+
+
+class ArticleInDB(BaseModel):
+
+    pass
+
+
+class ArticleResponse(BaseModel):
+
+    pass
